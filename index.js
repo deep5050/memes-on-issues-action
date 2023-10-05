52365ff1e064e9b51f935f110ecd4b845b0e89ea
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-05 09:44:03 +0300

    change memes to jokes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const Axios = require("axios");
 
 core.info("workflow started....");
 
@@ -22,39 +21,27 @@ if (!allow_owner || allow_owner == "false") {
   }
 }
 
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-async function getRandomMeme() {
-  const url =
-    "https://raw.githubusercontent.com/deep5050/programming-memes/main/memes.json";
+async function getRandomJoke() {
+  const url = "https://github-readme-jokes.vercel.app/api";
   try {
-    const response = await Axios.get(url);
-    var data = response.data;
-    var random = randomNumber(0, data.length);
-    var img_data = data[random];
-    var image_url =
-      "https://raw.githubusercontent.com/deep5050/programming-memes/main/" +
-      img_data["path"];
-    core.info(image_url);
-    var html = `\n<div><img alt=meme src=${image_url} width=500px ></div>\n`;
+    core.info(url);
+    var html = `\n<div><img alt=joke src=${url} width=500px ></div>\n`;
     return html;
   } catch (err) {
     core.setFailed(`Error:${err.message}`);
-    core.info(`Error in getting a meme: ${err.message}`);
+    core.info(`Error in getting a joke: ${err.message}`);
     return "";
   }
 }
 
-async function run(meme) {
+async function run(joke) {
   try {
     var event = github.context.eventName;
     var greetMsg;
     if (event === "pull_request" || event === "pull_request_target") {
       if (!PR_msg) {
         message =
-          "Hi, {{author}}, \nThanks for opening this PR :blue_heart: .\nContributors :people_holding_hands:  like you make the open source community :earth_africa:  such an amazing place to learn :book: , inspire :angel:, and create :art: .\nWe will review it :eyes: and get back to you as soon as possible :+1: . Just make sure you have followed the contribution guidelines.\n\nBy that time enjoy this meme :point_down: , hope you like it :smile:\n{{meme}}";
+          "Hi, {{author}}, \nThanks for opening this PR :blue_heart: .\nContributors :people_holding_hands:  like you make the open source community :earth_africa:  such an amazing place to learn :book: , inspire :angel:, and create :art: .\nWe will review it :eyes: and get back to you as soon as possible :+1: . Just make sure you have followed the contribution guidelines.\n\nBy that time enjoy this joke :point_down: , hope you like it :smile:\n{{joke}}";
         core.debug("PR msg not set, appying default");
       } else {
         message = PR_msg;
@@ -63,7 +50,7 @@ async function run(meme) {
     } else if (event === "issues") {
       if (!issue_msg) {
         message =
-          "Hi, {{author}}, \nThanks for your contribution :blue_heart: .\nContributors :people_holding_hands:  like you make the open source community :earth_africa:  such an amazing place to learn :book: , inspire :angel:, and create :art: .\nWe will investigate :eyes:  and get back to you as soon as possible :+1: . Just make sure you have given us sufficient information :information_source:.\n\nBy that time enjoy this meme :point_down: , hope you like it :smile:\n{{meme}}";
+          "Hi, {{author}}, \nThanks for your contribution :blue_heart: .\nContributors :people_holding_hands:  like you make the open source community :earth_africa:  such an amazing place to learn :book: , inspire :angel:, and create :art: .\nWe will investigate :eyes:  and get back to you as soon as possible :+1: . Just make sure you have given us sufficient information :information_source:.\n\nBy that time enjoy this joke :point_down: , hope you like it :smile:\n{{joke}}";
         core.debug("issue msg not set, applying default message");
       } else {
         message = issue_msg;
@@ -87,14 +74,14 @@ async function run(meme) {
 
     const octokit = github.getOctokit(github_token);
 
-    core.info(`got this meme: ${meme}`);
+    core.info(`got this joke: ${joke}`);
     core.info("commenting...");
     var messageBody = message
       .replace("{{author}}", "@" + author)
-      .replace("{{meme}}", meme);
+      .replace("{{joke}}", joke);
     messageBody =
       messageBody +
-      "\n\nUse [this action](https://github.com/deep5050/memes-on-issues-action) on your projects. Use [jokes on issues](https://github.com/deep5050/MastJokeMara) instead.";
+      "\n\nUse [this action](https://github.com/ivan-developer-01/jokes-on-issues-action) on your projects.";
 
     const comment = await octokit.issues.createComment({
       issue_number: issueNumber,
@@ -108,9 +95,9 @@ async function run(meme) {
   }
 }
 
-getRandomMeme()
+getRandomJoke()
   .then((data) => {
-    core.info(`meme: ${data}`);
+    core.info(`joke: ${data}`);
     run(data);
   })
   .catch((err) => {
